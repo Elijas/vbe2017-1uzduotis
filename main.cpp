3a68435032c1bc866c9b081c3f92a5b4622e791b
Elijas <master.elijas@gmail.com>
2017-07-03 16:13:57 +0300

    pass test output

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <cassert>
-#include <cstring>
 #include <sstream>
+#include <fstream>
 
+#define outputFile cout //D
 using namespace std;
 
 char getHexDigit(int decDigit) {
@@ -16,9 +16,29 @@ string getHexNumber(int decNumber) {
 }
 
 int main() {
-    std::cout << getHexNumber(16) << std::endl;
-    assert(getHexNumber(0) == "00");
-    assert(getHexNumber(128) == "80");
-    assert(getHexNumber(255) == "FF");
+    // Opening files and reading metadata
+    ifstream inputFile("U1.txt");
+    //ofstream outputFile("U1rez.txt"); //D
+    int length, width;
+    inputFile >> length >> width;
+
+    // Converting the numbers
+    for (int il = 0; il < length; ++il) {
+        for (int iw = 0; iw < width; ++iw) {
+            for (int i = 0; i < 3; ++i) {
+                int num;
+                inputFile >> num;
+                outputFile << getHexNumber(num);
+            }
+
+            if (iw < width - 1)
+                outputFile << ';';
+            else
+                outputFile << endl;
+        }
+    }
+
+
+
     return 0;
 }
